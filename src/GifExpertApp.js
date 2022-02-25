b957417c8d3dd4e8e3fdc17dfc41488600a33990
M-Francia <mariofranciarius@gmail.com>
2022-02-25 14:38:01 +0100

    Doc added

---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+
+const GifExpertApp = () => { 
+
+    const [categories, setCategories] = useState(['One punch']);
+    
+
+    return(<>
+        
+        <h2 className="animate__fadeIn"> GifExpertApp</h2>
+        <AddCategory setCategories={setCategories}/>
+        <hr />
+        <ol>
+            { 
+
+                categories.map( category => 
+                    <GifGrid 
+                        key={category}    
+                        category={category}
+                    />
+                ) 
+            
+            }
+        </ol>
+    </>);
+
+
+}
+
+export default GifExpertApp;
+
---
+++ src/components/GifGrid.js
@@ -0,0 +1,30 @@
+import React  from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
+//import { getGifts } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ({category}) => {
+
+
+    const {data:images , loading} = useFetchGifs(category);
+
+    //const [images, setImages] = useState([]);
+
+    return (
+    <>
+        <h3>{category}</h3>
+        
+        <div className='card-grid'>
+            {loading && <p>Loading...</p>}
+                {
+                    images.map( (img) =>{
+                        return <GifGridItem
+                            key={img.id} 
+                            { ...img }
+                        />
+                    })
+                }
+        </div>
+    </>
+  )
+}
